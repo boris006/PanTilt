ff1f19e7290c8bdc844e376d0c67bd7c051d3d3b
Maurice <99mg24@web.de>
2020-11-27 10:14:14 +0100

    hide nav bar

--- app/src/main/java/com/example/bt_firsttry/Timelapse.java
+++ app/src/main/java/com/example/bt_firsttry/Timelapse.java
@@ -68,6 +68,8 @@ public class Timelapse extends AppCompatActivity {
         useSensor();
         hideNavBar();
 
+        //hide nav bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
         mHolder = mSurfaceView.getHolder();
@@ -143,6 +145,20 @@ public class Timelapse extends AppCompatActivity {
         progress = null;
 
 
+    }
+    //hide nav bar on focus change
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
 
@@ -395,7 +411,7 @@ public class Timelapse extends AppCompatActivity {
 //        }
     }
     public void hideNavBar(){
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
 
     }
 }
